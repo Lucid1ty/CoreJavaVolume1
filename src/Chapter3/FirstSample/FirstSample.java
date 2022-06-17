74363247d7a6eb77a942765fb1a9a1b8fd9eb215
Lucidity <2993514952@qq.com>
2022-06-17 17:23:39 +0800

    add some code

--- src/Chapter3/FirstSample/FirstSample.java
+++ src/Chapter3/FirstSample/FirstSample.java
@@ -1,8 +1,10 @@
 package Chapter3.FirstSample;
-/**
- * This is the first sample program in Core Java Chapter 3
- * @version 1.01 1997-03-22
- * @author Gary Cornell
+
+// * This is the first sample program in Core Java Chapter 3
+// 一个简单的 Java 应用程序
+/*
+    1. Java区分大小写
+
  */
 public class FirstSample {
     public static void main(String[] args) {
